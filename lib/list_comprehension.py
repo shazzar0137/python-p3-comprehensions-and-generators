392c3935ccd308bec1549d7b1eabb49b5d1e450a
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-05-11 21:30:59 +0300

    list expression

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,8 @@
 
 def return_evens(num_list):
     pass
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
     pass
+    return [sentence + '!' for sentence in sentence_list]
